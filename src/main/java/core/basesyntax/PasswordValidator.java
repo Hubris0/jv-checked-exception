77c457e48fa87e8dfb8783ee99a9c42524242036
user <pawel.m.janik@gmail.com>
2026-06-30 14:05:46 +0200

    Code formatting

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,13 +1,14 @@
 package core.basesyntax;
 
 public class PasswordValidator {
-    public void validate(String password, String repeatPassword) throws PasswordValidationException {
+    public void validate(String password, String repeatPassword)
+            throws PasswordValidationException {
         if (repeatPassword == null || password == null) {
-            throw new PasswordValidationException("Your passwords are incorrect. Try again.");
+            throw new PasswordValidationException("Your passwords are incorrect. Try again.\n");
         } else if (!password.equals(repeatPassword)) {
-            throw new PasswordValidationException("Wrong passwords");
-        }else if (password.length() < 10) {
-            throw new PasswordValidationException("Your passwords are incorrect. Try again.");
+            throw new PasswordValidationException("Wrong passwords\n");
+        } else if (password.length() < 10) {
+            throw new PasswordValidationException("Your passwords are incorrect. Try again.\n");
         }
     }
 }
